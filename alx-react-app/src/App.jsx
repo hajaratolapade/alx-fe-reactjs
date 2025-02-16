02b94391809fe52741cdac66a001e9e86edea0dd
hajaratolapade <olapadehajarat@outlook.com>
2025-02-16 22:09:27 +0100

    Fixed App component duplication and added WelcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
 
 function App() {
     return (
         <div>
+            <Header />
             <WelcomeMessage />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
