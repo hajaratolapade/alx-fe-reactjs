ee99014916b149d6a1954e7e6c6390ed6b9541ca
hajaratolapade <olapadehajarat@outlook.com>
2025-02-18 12:15:52 +0100

    Added props where required

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 const UserProfile = ({ name, age, bio }) => {
   return (
     <div>
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
