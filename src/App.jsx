73e8d8e99f90945fc0eab5095a7c5db746d7b406
hajaratolapade <olapadehajarat@outlook.com>
2025-02-19 13:59:13 +0100

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,21 @@ import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 import UserProfile from "./components/UserProfile";
 import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 import "./App.css";
 import Footer from "./components/Footer";
 
-function App() {
-    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-    return (
-        <>
+    const App = () => {
+        return (
+            <div className="App">
             <Header />
             <MainContent />
             <WelcomeMessage />
-            <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+            <ProfilePage />
             <UserProfile name= "Alice" age= "25" bio= "Loves hiking and photography" />
             <Footer />
-            </>
+            </div>
     );
-}
+};
 
 export default App;
