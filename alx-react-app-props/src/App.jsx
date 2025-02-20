dbff75a4acf0b9aeb624c69f417eaa0958bb1252
hajaratolapade <olapadehajarat@outlook.com>
2025-02-20 17:05:00 +0100

    implemented context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
+import ProfilePage from './ProfilePage';
 import UserProfile from "./components/UserProfile";
 import "./App.css";
 import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 
 function App() {
-    return (
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+    return <ProfilePage userData={userData} />;
         <>
             <Header />
             <MainContent />
@@ -16,7 +19,6 @@ function App() {
             <UserProfile name= "Alice" age= "25" bio= "Loves hiking and photography" />
             <Footer />
             </>
-    );
 }
 
 export default App;
