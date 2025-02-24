a0e1b3c7a4a481b19053a73942bb391ac9b53836
hajaratolapade <olapadehajarat@outlook.com>
2025-02-24 15:30:00 +0100

    Routing Implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeDetails from './components/RecipeDetails'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
@@ -9,20 +9,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Routes>
+    <Router> {/* Wrap Routes with Router */}
+      <Routes>
         <Route path="/" element={<RecipeList />} /> 
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
         <Route path="/add-recipe" element={<AddRecipeForm />} />
       </Routes>
-
-      <div className="card">
-      </div>
-      <p className="read-the-docs">
-      </p>
-
-      <AddRecipeForm />
-    </>
+    </Router>
   )
 }
 
