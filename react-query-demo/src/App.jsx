8c9b206bcd3e83d1e4c3345de767097bd95c5326
hajaratolapade <olapadehajarat@outlook.com>
2025-03-05 12:36:17 +0100

    Created a component to fetch data

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const queryClient = new QueryClient();
 
   return (
     <>
@@ -28,8 +30,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
     </>
   )
-}
+};
 
-export default App
+export default App;
