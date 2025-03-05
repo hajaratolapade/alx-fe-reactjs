0bf89fca8a2465cd972cf92a1c04e4a0a0fad34b
hajaratolapade <olapadehajarat@outlook.com>
2025-03-05 12:02:46 +0100

    Implemented Feature for Catching and Updating Data

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,9 +1,19 @@
+import PostsComponent from "./components/PostsComponent";
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css'
 
   const queryClient = new QueryClient();
+  const App = () => {
 
   function App() {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">React Query Demo</h1>
+        <PostsComponent />
+      </div>
+      );
+    };
+
     return (
       <QueryClientProvider client={queryClient}>
         <PostsComponent />
