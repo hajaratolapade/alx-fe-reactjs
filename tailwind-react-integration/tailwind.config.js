d35dea51567a3aa1a51e1c4c42153807aab14c7e
HajaratOlapade <olapadehajarat@outlook.com>
2025-03-16 06:34:57 +0100

    Added tailwind-react-integration folder

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,5 @@
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const darkMode = false;
+export const theme = { extend: {} };
+export const variants = { extend: {} };
+export const plugins = [];
