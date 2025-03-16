c094449101381eaf925361f8298a8dfd56f0aefd
HajaratOlapade <olapadehajarat@outlook.com>
2025-03-16 07:16:11 +0100

    Updated tailwind.config.js to include purge, darkMode, and variants

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,12 @@
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Old syntax (now replaced by "content")
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // New syntax
+  darkMode: "class", // Can be "media" or "class"
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}, // You can define custom variants here if needed
+  },
+  plugins: [],
+};
